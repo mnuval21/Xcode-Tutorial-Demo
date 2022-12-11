2f1c5a7c7be0140e4511c20804d637bedfaaa2fa
mnuval21 <mnuval21@gmail.com>
2022-12-11 14:51:29 -0500

    Initial Commit

---
+++ Xcode Tutorial Demo/Xcode_Tutorial_DemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  Xcode_Tutorial_DemoApp.swift
+//  Xcode Tutorial Demo
+//
+//  Created by Melissa Nuval on 12/11/22.
+//
+
+import SwiftUI
+
+@main
+struct Xcode_Tutorial_DemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
